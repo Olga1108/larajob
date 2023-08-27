a922ebb07214317fe87c4e8630e34bbf062c705f
Olha Razvaliaieva <olha.pushkar11@gmail.com>
2023-08-27 17:08:31 +0300

    feat: add payment stripe

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,25 +23,39 @@ class UserController extends Controller
 
     public function storeSeeker(RegistrationFormRequest $request)
     {
-        User::create([
+        $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
             'password' => bcrypt(request('password')),
             'user_type' => self::JOB_SEEKER,
         ]);
-        return redirect()->route('login')->with('successMessage', 'Your account was created');
+
+        Auth::login($user);
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json('success');
+
+        //return redirect()->route('verification.notice')->with('successMessage', 'Your account was created');
     }
 
     public function storeEmployer(RegistrationFormRequest $request)
     {
-        User::create([
+        $user = User::create([
             'name' => request('name'),
             'email' => request('email'),
             'password' => bcrypt(request('password')),
             'user_type' => self::JOB_POSTER,
             'user_trial' => now()->addWeek()
         ]);
-        return redirect()->route('login')->with('successMessage', 'Your account was created');
+
+        Auth::login($user);
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json('success');
+
+        //return redirect()->route('verification.notice')->with('successMessage', 'Your account was created');
     }
 
     public function login()
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SubscriptionController;
+use App\Http\Controllers\PostJobController;
+use App\Http\Middleware\isEmployer;
+use App\Http\Middleware\isPremiumUser;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +24,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/login');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
 Route::get('/register/seeker', [UserController::class, 'createSeeker'])->name('create.seeker');
 Route::get('/register/employer', [UserController::class, 'createEmployer'])->name('create.employer');
 Route::post('/register/seeker', [UserController::class, 'storeSeeker'])->name('store.seeker');
@@ -29,4 +40,19 @@ Route::post('/login', [UserController::class, 'postLogin'])->name('login.post');
 
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware('verified')
+    ->name('dashboard');
+
+Route::get('/resend/verification/email', [DashboardController::class, 'resend'])->name('resend.email');
+
+Route::get('/verify', [DashboardController::class, 'verify'])->name('verification.notice');
+
+Route::get('subscribe', [SubscriptionController::class, 'subscribe'])->name('subscribe');
+Route::get('pay/weekly', [SubscriptionController::class, 'initiatePayment'])->name('pay.weekly');
+Route::get('pay/monthly', [SubscriptionController::class, 'initiatePayment'])->name('pay.monthly');
+Route::get('pay/yearly', [SubscriptionController::class, 'initiatePayment'])->name('pay.yearly');
+Route::get('payment/success', [SubscriptionController::class, 'paymentSuccess'])->name('payment.success');
+Route::get('payment/cancel', [SubscriptionController::class, 'cancel'])->name('payment.cancel');
+
+Route::get('job/create', [PostJobController::class, 'create'])->name('job.create')->middleware(isPremiumUser::class);
